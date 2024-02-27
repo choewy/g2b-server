274c85e9739462a0b7169521b7ecc2b1808d5f6a
choewy <choewy32@gmail.com>
2024-02-27 22:38:50 +0900

    feature/bootstrap pipes, interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import cookieParser from 'cookie-parser';
 import { json, urlencoded } from 'express';
 
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { BadRequestException, ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -26,6 +27,27 @@ async function bootstrap() {
   app.useWebSocketAdapter(new IoAdapter(app));
   app.enableCors(configFactory.corsOptions);
   app.useGlobalFilters(new AppFilter());
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(new Reflector(), {
+      enableCircularCheck: true,
+      enableImplicitConversion: true,
+    }),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableCircularCheck: true,
+        enableImplicitConversion: true,
+      },
+      stopAtFirstError: true,
+      exceptionFactory: (errors) => {
+        throw new BadRequestException(Object.values(errors.shift()?.constraints)?.pop(), {
+          description: 'Validate Error',
+        });
+      },
+    }),
+  );
 
   await app.listen(4000);
 }
