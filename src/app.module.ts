8274548fbc60f17590d2147b26a77f6f1435f414
choewy <choewy32@gmail.com>
2024-03-20 20:10:32 +0900

    feature/bootstrap

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
-import { AwsConfig, EmailConfig, JwtConfig, OpenApiConfig, ServerConfig, TypeOrmConfig } from '@common/configs';
+import { AwsConfig, EmailConfig, JWT_CONFIG, JwtConfig, OpenApiConfig, ServerConfig, TYPEORM_CONFIG, TypeOrmConfig } from '@common/configs';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,6 +12,18 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [ServerConfig, TypeOrmConfig, JwtConfig, AwsConfig, EmailConfig, OpenApiConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory(config: ConfigService) {
+        return config.get(TYPEORM_CONFIG);
+      },
+    }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory(config: ConfigService) {
+        return config.get(JWT_CONFIG);
+      },
     }),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createBootstrapOptions } from '@choewy/nestjs-bootstrap';
 import { WinstonLoggerFactory } from '@choewy/nestjs-winston';
+import { SERVER_CONFIG, ServerOption } from '@common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -10,11 +12,14 @@ async function bootstrap() {
   const logger = loggerFactory.create({ fileLevel: ['verbose', 'warn', 'error'] });
 
   const app = await NestFactory.create(AppModule, { logger });
+  const config = app.get(ConfigService);
+  const serverConfig = config.get<ServerOption>(SERVER_CONFIG);
 
+  app.enableCors(serverConfig.cors);
   app.useGlobalInterceptors(...bootstrapOptions.interceptors);
   app.useGlobalPipes(...bootstrapOptions.pipes);
   app.useGlobalFilters(...bootstrapOptions.filters);
 
-  await app.listen(3000);
+  await app.listen(serverConfig.listen.port, serverConfig.listen.host);
 }
 bootstrap();
